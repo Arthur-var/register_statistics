00afd09552639fc17ccaca8486c7fb0d2117995e
Arthur-var <arturchik.var@gmail.com>
2026-03-02 19:39:31 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+
+    pool_size=20,          
+    max_overflow=30,      
+    pool_timeout=30,   
+    pool_recycle=1800,   
+    pool_pre_ping=True,   
+
+    echo=False
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
